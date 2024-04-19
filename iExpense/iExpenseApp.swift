730adac8e473b73f3c80da7006df9b1d90f66de6
Justine Kenji Dela Cruz <86695006+jkenjidc@users.noreply.github.com>
2024-04-19 11:37:00 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kenji Dela Cruz on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
